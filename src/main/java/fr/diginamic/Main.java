6fc5847554fc0872bd711f30ae7918ae8a44c55c
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-07-03 00:46:48 +0200

    Parser fonctionnel

--- src/main/java/fr/diginamic/Main.java
+++ src/main/java/fr/diginamic/Main.java
@@ -4,6 +4,7 @@ package fr.diginamic;
 import fr.diginamic.ihm.Ihm;
 import fr.diginamic.mochizukiTools.Params;
 import fr.diginamic.mochizukiTools.Utils;
+import fr.diginamic.service.IboofManager;
 import fr.diginamic.service.Logging;
 import fr.diginamic.service.Cleaner;
 import jakarta.persistence.EntityManager;
@@ -12,27 +13,17 @@ import jakarta.persistence.Persistence;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedLookAndFeelException, IOException {
         Utils.clearConsole();
         Params.welcomePrompt();
 
-        Utils.msgDebug("Parsing du fichier csv");
-        Utils.msgResult(String.format("Parsing du fichier CSV <%s> OK", Cleaner.CSV_FILE_RELATIVE_PATH));
+        String fileTestPath = ResourceBundle.getBundle("project").getString("project.TEST_csvfile");
+        I_IboofManager iboofManager = new IboofManager(fileTestPath);
+        iboofManager.mapCsvFileToDatabase();
 
-        Utils.msgInfo("Début du mapping JPA de la base de données");
-        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("IBOOF-JPA");
-             EntityManager em = emf.createEntityManager();
-        ) {
-            Utils.msgInfo("Mapping de la base de données OK");
-            Utils.msgInfo("Connection à la base de données OK");
-        } catch (IllegalStateException e) {
-            Logging.LOG.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-
-        Utils.msgInfo("Lancement de la boucle évenementielle");
-        Ihm.display();
+        //Ihm.display();
     }
 }
